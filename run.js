889004d9b78c7bcca7507aadac68c639d5cfc267
Chamod Keshan <98629920+ChamodKeshan@users.noreply.github.com>
2022-10-09 15:44:14 +0530

    Add files via upload

--- run.js
+++ run.js
@@ -2133,13 +2133,12 @@ break
 	    case 'song': case 'ytmp3': case 'audio': {
 	            oh = `⛔ *INVAID DOWNLOADED*`
                 if (!text) return reply(`⛔*Enter name.*\nකරුණාකර සබැදියක් හෝ නමක් ඇතුලත් කරන්න.*`)
-                await axios
-      .get(`https://zenzapis.xyz/downloader/y2mate?apikey=hdiiofficial&query=${text}`)
+                await axios.get(`https://zenzapis.xyz/downloader/y2mate?apikey=hdiiofficial&query=${text}`)
       .then(async (response) => {
         const {
           link,
         } = response.data.result
-        const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+        const videoBuffer = await axios.get(link, )
                 XeonBotInc.sendMessage(m.chat, { audio: { url: videoBuffer.data }, mimetype: 'audio/mpeg', fileName: `${data.result.title}.mp3` }, { quoted: m }).catch ((err) => reply(oh))
              })
              }
@@ -2149,7 +2148,7 @@ break
                 if (!text) return reply(`⛔*Enter name.*\nකරුණාකර සබැදියක් හෝ නමක් ඇතුලත් කරන්න.*`)
                 axios.get(`https://zenzapis.xyz/downloader/y2mate?apikey=hdiiofficial&query=lelena`)
 					.then(({data}) => {
-					if (data.status == false) return reply(`⛔*NOT FOUND`)
+					if (data.status == false) return reply(`⛔*NOT FOUND*`)
 					XeonBotInc.sendMessage(m.chat, { audio: { url: data.result.getVideo }, mimetype: 'video/mp4', fileName: `${data.result.title}.mp4` }, { quoted: m }).catch ((err) => reply(oh))
             })
             }
